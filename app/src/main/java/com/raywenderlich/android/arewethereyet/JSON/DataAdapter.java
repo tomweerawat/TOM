604e1d2dde36373c7d98c47c884cd0821187149c
weerawatcomsci <tomweerawat.cs@gmail.com>
2016-05-26 12:31:33 +0700

    tomtest1

--- app/src/main/java/com/raywenderlich/android/arewethereyet/JSON/DataAdapter.java
+++ app/src/main/java/com/raywenderlich/android/arewethereyet/JSON/DataAdapter.java
@@ -1,5 +1,11 @@
 package com.raywenderlich.android.arewethereyet.JSON;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.widget.TextView;
 
 import android.support.v7.widget.RecyclerView;
@@ -7,13 +13,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import android.app.Activity;
+import com.raywenderlich.android.arewethereyet.AllGeofencesActivity;
+import com.raywenderlich.android.arewethereyet.MapsActivity;
 import com.raywenderlich.android.arewethereyet.R;
 
 import java.util.ArrayList;
 
-public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
+public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder>{
     private ArrayList<AndroidVersion> android;
+    private Context context;
 
     public DataAdapter(ArrayList<AndroidVersion> android) {
         this.android = android;
@@ -27,14 +37,28 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(DataAdapter.ViewHolder viewHolder, int i) {
-
+        final AndroidVersion androidVersion = android.get(i);
         viewHolder.tv_name.setText(android.get(i).getName());
         //viewHolder.tv_name.setText(android.get(i).getUsername());
-       // viewHolder.tv_name.setText(android.get(i).getPassword());
+        // viewHolder.tv_name.setText(android.get(i).getPassword());
         //viewHolder.tv_name.setText(android.get(i).getEmail());
-
+        Log.d("onBindViewHolder","onBindViewHolder");
         viewHolder.tv_version.setText(android.get(i).getVer());
         viewHolder.tv_api_level.setText(android.get(i).getApi());
+
+       /* viewHolder.setClickListener(new ItemClickListener() {
+            @Override public void onClickItem(int pos) {
+                Log.e("clickitem","clickitem"+getItemId(pos));
+                // System.exit(0);
+                //Toast.makeText(context, "CLICK : " + androidVersion.getApi(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override public void onLongClickItem(int pos) {
+                //Toast.makeText(context, "LONG CLICK : " + androidVersion.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
     }
 
     @Override
@@ -42,8 +66,10 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
         return android.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+
+    public class ViewHolder extends RecyclerView.ViewHolder   implements View.OnClickListener, View.OnLongClickListener  {
         private TextView tv_name, tv_version, tv_api_level;
+        private ItemClickListener mListener;
 
         public ViewHolder(View view) {
             super(view);
@@ -51,7 +77,42 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
             tv_name = (TextView) view.findViewById(R.id.tv_name);
             tv_version = (TextView) view.findViewById(R.id.tv_version);
             tv_api_level = (TextView) view.findViewById(R.id.tv_api_level);
+            view.setOnClickListener(this);
+            view.setOnLongClickListener(this);
+        }
+        public void setClickListener(ItemClickListener listener) {
+            this.mListener = listener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.d("Click","Click");
+            Toast.makeText(v.getContext(), "OnClick Version :", Toast.LENGTH_SHORT).show();
+
+            //Intent i = new Intent(v.getContext(), MapsActivity.class);
+            //Intent i = new Intent(context,DataAdapter.this,MapsActivity.class);
+            //context.startActivity(i);
+
+
+
+
+
+            //mListener.onClickItem(getLayoutPosition());
+            // System.exit(0);
 
         }
+
+        @Override
+        public boolean onLongClick(View v) {
+            mListener.onLongClickItem(getLayoutPosition());
+            return false;
+        }
     }
+    public interface ItemClickListener {
+        void onClickItem(int pos);
+
+        void onLongClickItem(int pos);
+    }
+
+
 }
--- app/src/main/java/com/raywenderlich/android/arewethereyet/JSON/MainActivity.java
+++ app/src/main/java/com/raywenderlich/android/arewethereyet/JSON/MainActivity.java
@@ -1,27 +1,37 @@
 package com.raywenderlich.android.arewethereyet.JSON;
 
-import android.app.Application;
-
-import android.support.v7.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.raywenderlich.android.arewethereyet.Hello;
 import com.raywenderlich.android.arewethereyet.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import butterknife.Bind;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+    @OnClick(R.id.fab) void cc(){
+        Intent i = new Intent(this, Hello.class);
+        startActivity(i);
+    }
     private Integer[] imgid = {R.mipmap.ic_launcher,};
 
     private RecyclerView recyclerView;
@@ -32,17 +42,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         initViews();
 
+
     }
-    private void initViews(){
-        recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
+
+    private void initViews() {
+        recyclerView = (RecyclerView) findViewById(R.id.card_recycler_view);
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
+
         loadJSON();
     }
-    private void loadJSON(){
+
+    private void loadJSON() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.56.1:8181")//("http://api.learn2crack.com")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -55,18 +70,23 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<JSONResponse> call, Response<JSONResponse> response) {
 
                 JSONResponse jsonResponse = response.body();
-                Log.d("Datafromserver",jsonResponse+"");
+                Log.d("Datafromserver", jsonResponse + "");
                 data = new ArrayList<>(Arrays.asList(jsonResponse.getAndroid()));
-                Log.e("data",jsonResponse.getAndroid()+"");
+                Log.e("data", jsonResponse.getAndroid() + "");
                 adapter = new DataAdapter(data);
                 recyclerView.setAdapter(adapter);
             }
 
             @Override
             public void onFailure(Call<JSONResponse> call, Throwable t) {
-                Log.d("Error",t.getMessage());
+                Log.d("Error", t.getMessage());
             }
         });
+    }
+
+
+    @OnClick(R.id.fab)
+    public void onClick() {
     }
 }
 
